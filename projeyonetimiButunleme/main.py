9ab179b822224cda2b8fcd03d5a46df18f27a70a
MERVE SÖYLEMEZ <merve.soylmz@gmail.com>
2021-06-22 19:18:54 +0300

    Platformların aşağıya kayması sağlandı ve yeni platformlar eklendi.

--- projeyonetimiButunleme/main.py
+++ projeyonetimiButunleme/main.py
@@ -16,9 +16,17 @@ class Game:
 
         p1 = Platform(0, HEIGHT-30, WIDTH, 40)
         p2 = Platform(WIDTH/2-50, 500, 100, 30)
+        p3 = Platform(400, 300, 50, 30)
+        p4 = Platform(300, 200, 30, 30)
+        p5 = Platform(100, 200, 70, 30)
+        p6 = Platform(50, 500, 100, 30)
 
         self.platforms.add(p1)
         self.platforms.add(p2)
+        self.platforms.add(p3)
+        self.platforms.add(p4)
+        self.platforms.add(p5)
+        self.platforms.add(p6)
 
 
         self.player = Player(self)
@@ -27,6 +35,11 @@ class Game:
         self.all_sprites.add(self.player)
         self.all_sprites.add(p1)
         self.all_sprites.add(p2)
+        self.all_sprites.add(p3)
+        self.all_sprites.add(p4)
+        self.all_sprites.add(p5)
+        self.all_sprites.add(p6)
+
 
 
         self.run()
@@ -52,11 +65,18 @@ class Game:
                 self.player.hiz.y = 0
                 self.player.rect.bottom = carpismalar[0].rect.top
 
-        if self.player.hiz.y < 0:
-            carpismalar = pygame.sprite.spritecollide(self.player, self.platforms, dokill = False)
-            if carpismalar:
-                self.player.hiz.y = 0
-                self.player.rect.top = carpismalar[0].rect.bottom
+        if self.player.rect.top < HEIGHT/4:
+            self.player.rect.y += abs(self.player.hiz.y)
+            for plat in self.platforms:
+                plat.rect.y += abs(self.player.hiz.y)
+                if plat.rect.top >= HEIGHT:
+                    plat.kill()
+
+        while len(self.platforms) < 6:
+            genislik = random.randrange(50, 100)
+            p = Platform(random.randrange(0, WIDTH-genislik), random.randrange(-75, -20), genislik, 30)
+            self.platforms.add(p)
+            self.all_sprites.add(p)
 
         pygame.display.update()
 
